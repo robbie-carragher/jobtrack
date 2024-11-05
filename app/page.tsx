4cfd3c4e11e9e4116df226c127e7453712615a4a
robbie-carragher <orcafinweb@gmail.com>
2024-11-05 11:45:32 -0800

    global font changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 
-import { Image } from 'next/image';
-import { Logo } from '../assets/logo.svg';
-import { LandingImg } from '../assets/main.svg';
+import Image from 'next/image';
+import Logo from '../assets/logo.svg';
+import LandingImg from '../assets/main.svg';
 import { Button } from '@/components/ui/button';
-import { Link } from 'next/link';
+import Link from 'next/link';
 import { Camera } from 'lucide-react';
 
 export default function Home() {
